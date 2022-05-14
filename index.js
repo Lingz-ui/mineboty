c189a4100c37cbab9410d4bbd59a90f6d339aa15
Texas <79846517+Lingz-ui@users.noreply.github.com>
2022-05-14 10:14:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const express = require("express");
+const app = express()
+var fs = require('fs')
+
+app.get('/', (req, res) => {
+  res.send("Use this link for uptime")
+
+})
+app.get('/config', (req, res) => {
+  res.json(fs.readFileSync("./config.json"))
+})
+app.listen(3000, () => {
+  console.log("bot started")
+})
+
 const {mineboty} = require("mineboty")
 mineboty();
